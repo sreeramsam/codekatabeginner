4ec50cc494436a6f92691dfdd9c8b3df9fffde43
sreeramsam <52190534+sreeramsam@users.noreply.github.com>
2019-07-09 12:26:23 +0530

    Create oddoreven.cpp

---
+++ oddoreven.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin>>n;
+	if(n<0)
+	cout<<"invalid";
+	else
+	{
+		if(n%2==0)
+		{
+		cout<<"Even";
+		}
+		else 
+		{
+		cout<<"Odd";
+		}
+	}
